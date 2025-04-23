759b47d8e46082d869e23697e056e9815f7a5a10
17joeluffy <117495211+17joeluffy@users.noreply.github.com>
2025-04-23 21:32:49 +0530

    lex

---
+++ input.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+   // int a;
+    int b;
+    printf("Enter the input num: ");
+    //scanf("%d",a);
+
+    printf("Enter the another number: ");
+    //scanf("%d",b);
+      int sum = a+b;
+      printf("Sum: %d", sum);
+
+}
--- lex.yy.c
+++ lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
+#define YY_NUM_RULES 4
+#define YY_END_OF_BUFFER 5
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,9 +360,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[10] =
+static const flex_int16_t yy_accept[13] =
     {   0,
-        0,    0,    4,    2,    1,    0,    1,    1,    0
+        0,    0,    5,    3,    3,    0,    1,    0,    0,    1,
+        2,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -371,8 +372,8 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    3,    1,    4,    4,    4,
-        4,    4,    4,    4,    4,    4,    4,    1,    1,    1,
+        1,    3,    1,    1,    1,    1,    4,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -399,29 +400,31 @@ static const YY_CHAR yy_ec[256] =
 
 static const YY_CHAR yy_meta[5] =
     {   0,
-        1,    1,    1,    2
+        1,    2,    1,    1
     } ;
 
-static const flex_int16_t yy_base[11] =
+static const flex_int16_t yy_base[15] =
     {   0,
-        0,    0,    8,    9,    2,    0,    0,    0,    9,    5
+        0,    0,   13,   14,    2,    9,    0,    8,    6,    0,
+       14,   14,    6,    8
     } ;
 
-static const flex_int16_t yy_def[11] =
+static const flex_int16_t yy_def[15] =
     {   0,
-        9,    1,    9,    9,    9,   10,    5,   10,    0,    9
+       12,    1,   12,   12,   12,   13,   14,   13,    8,   14,
+       12,    0,   12,   12
     } ;
 
-static const flex_int16_t yy_nxt[14] =
+static const flex_int16_t yy_nxt[19] =
     {   0,
-        4,    4,    4,    5,    6,    7,    8,    9,    3,    9,
-        9,    9,    9
+        4,    4,    4,    5,    6,    7,    8,    8,   10,   11,
+        9,    9,   12,    3,   12,   12,   12,   12
     } ;
 
-static const flex_int16_t yy_chk[14] =
+static const flex_int16_t yy_chk[19] =
     {   0,
-        1,    1,    1,    1,    5,    5,   10,    3,    9,    9,
-        9,    9,    9
+        1,    1,    1,    1,    5,    5,   13,   13,   14,    9,
+        8,    6,    3,   12,   12,   12,   12,   12
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -438,12 +441,13 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "q10.l"
-#line 2 "q10.l"
-
-#include<stdio.h>
-#line 445 "lex.yy.c"
-#line 446 "lex.yy.c"
+#line 1 "q13.l"
+#line 5 "q13.l"
+#include <stdio.h>
+int comment_lines = 0;
+FILE *out;
+#line 449 "lex.yy.c"
+#line 450 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -660,10 +664,10 @@ YY_DECL
 		}
 
 	{
-#line 6 "q10.l"
+#line 10 "q13.l"
 
 
-#line 666 "lex.yy.c"
+#line 670 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -690,13 +694,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 10 )
+				if ( yy_current_state >= 13 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 9 );
+		while ( yy_base[yy_current_state] != 14 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -722,21 +726,39 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 8 "q10.l"
-{printf("Number found: %s\n", yytext);}
+#line 12 "q13.l"
+{
+    comment_lines++;
+    // Don’t copy this to output
+}
 	YY_BREAK
 case 2:
 /* rule 2 can match eol */
 YY_RULE_SETUP
-#line 9 "q10.l"
-{}
+#line 17 "q13.l"
+{
+    // Count newlines inside multiline comment
+    for (int i = 0; yytext[i] != '\0'; i++) {
+        if (yytext[i] == '\n') comment_lines++;
+    }
+    // Don't copy to output
+}
 	YY_BREAK
 case 3:
+/* rule 3 can match eol */
 YY_RULE_SETUP
-#line 11 "q10.l"
+#line 25 "q13.l"
+{
+    // Copy all other characters to output
+    fputc(yytext[0], out);
+}
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 30 "q13.l"
 ECHO;
 	YY_BREAK
-#line 739 "lex.yy.c"
+#line 761 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1033,7 +1055,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 10 )
+			if ( yy_current_state >= 13 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1061,11 +1083,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 10 )
+		if ( yy_current_state >= 13 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 9);
+	yy_is_jam = (yy_current_state == 12);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1741,23 +1763,46 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 11 "q10.l"
+#line 30 "q13.l"
 
 
-int main(int argc, char *argv[]){
-if(argc > 1){
-FILE *file= fopen(argv[1],"r");
- if(!file){
-  perror("Cannot open file");
-return 1;
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s input.c output.c\n", argv[0]);
+        return 1;
+    }
+
+    FILE *in = fopen(argv[1], "r");
+    if (!in) {
+        perror("Error opening input file");
+        return 1;
+    }
+
+    out = fopen(argv[2], "w");
+    if (!out) {
+        perror("Error opening output file");
+        fclose(in);
+        return 1;
+    }
+
+    yyin = in;
+    yylex();  // Start lexical analysis
+
+    printf("Total comment lines removed: %d\n", comment_lines);
+
+    fclose(in);
+    fclose(out);
+    return 0;
 }
-yyin = file;
 
-}
-yylex();
-return 0;
-}
-
-int yywrap(){
+int yywrap() {
     return 1;
 }
+
+
+
+
+
+
+
+
